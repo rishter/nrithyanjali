05f8c452e6aa3acef8e14720ffbed8eb5388615a
Rishikesh Tirumala <rishter@gmail.com>
2013-01-14 16:08:26 -0500

    add readme

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Nrithyanjali::Application.config.secret_token = '01404fb76da299e8ee6a2b2f0b04ccc6f2ff4ee8fae1a0e75fe7ad2bfd1d020a1ace46226c300ad6f1ed0dadb50c272de60812523c37555826cc7fc7cd874566'
